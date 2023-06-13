cd34b317efa15fb8c2892c3967a3a9c92c8902cb
kyle138 <kyle138@gmail.com>
2023-06-12 22:50:39 -0500

    Added a textarea for messages.

--- js/timekeeper.js
+++ js/timekeeper.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const hrsWrkd = document.getElementById("inputHoursWorked");
   const clckdIn = document.getElementById("inputClockedIn");
   const timeBtn = document.getElementById("reckonTime");
+  const msgArea = document.getElementById("messageArea");
 
   // Initialize
   var fieldsValid = {
@@ -117,6 +118,20 @@ document.addEventListener("DOMContentLoaded", () => {
       console.log("Already over 80");
     } else {
       // ********************* do something here
+      // This case shouldn't be possible due to validation but test for it anyways
+      if(hrsWrkd.value < 0) {
+        console.log("How you worked negative hours? ");
+      } else if(hrsWrkd.value > 80) { // Already over 80 hours
+        console.log("You are already over 80 hours. Go home, relax.");
+      } else if(hrsWrkd.value < 40) { // Less than 40, assume user is targeting 40 hours.
+        console.log("Must be week one.");
+        console.log(40-hrsWrkd.value);
+        console.log(80-hrsWrkd.value);
+        msgArea.innerHTML = "Must be week one.";
+      } else if(hrsWrkd.value < 80) {
+        console.log("Must be in week two.");  // Between 40 and 80 hours, assume targeting 80 hours.
+        console.log(80-hrsWrkd.value);
+      }
       console.log("something about missing fields");
     }
   } // End reckonTime
