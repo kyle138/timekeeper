01b5309376c09ddcea5817203c9565d47e3c37f1
kyle138 <kyle138@gmail.com>
2023-07-13 10:52:45 -0500

    testing scrollto output

--- js/timekeeper.js
+++ js/timekeeper.js
@@ -135,6 +135,13 @@ document.addEventListener("DOMContentLoaded", () => {
   // Function reckonTime
   function reckonTime() {
     console.log("Reckoning...");
+    // Scroll to output
+    console.log(msgArea.offsetTop);
+    window.scrollTo({
+      left: 0,
+      top: msgArea.offsetTop,
+      behavior: "smooth" 
+    });
     // var hrsWrkdRnd = Math.
     if(hrsWrkd.value >= 80) {
       console.log("Already over 80");
